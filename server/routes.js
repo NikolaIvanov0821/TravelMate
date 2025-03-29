de9942a896b5563a7cda55a5a4300403b9e6e282
NikolaIvanov0821 <ni.ivanov2008@gmail.com>
2025-03-29 19:30:48 +0200

    clean the server

--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,9 @@
 import { Router } from "express";
-import productController from "./controllers/productsController.js";
 import userController from "./controllers/userController.js";
-import reviewsController from "./controllers/reviewsController.js";
-
 
 const routes = Router();
 
-routes.use('/products', productController)
 routes.use('/users', userController)
-routes.use('/reviews', reviewsController)
 
 
 export default routes;
