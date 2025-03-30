051207ea084d5ddc5936288e6b8d07bdd2d50eb5
NikolaIvanov0821 <ni.ivanov2008@gmail.com>
2025-03-30 12:28:34 +0300

    Update index.js again

--- server/index.js
+++ server/index.js
@@ -7,8 +7,9 @@ import cors from "cors";
 
 console.log('It works!');
 
+const MONGO_URI = 'mongodb+srv://niivanov2008:niivanov2008@cluster0.dehwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 try {
-    await mongoose.connect('mongodb+srv://niivanov2008:niivanov2008@cluster0.dehwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { dbName: 'travelmate'})
+    await mongoose.connect(MONGO_URI, { dbName: 'travelmate'})
     console.log('DB Connected!');
 } catch (error) {
     console.log('Cannot connect to DB!');
