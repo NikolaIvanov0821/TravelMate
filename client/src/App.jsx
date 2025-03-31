59d7291b81223baefeab8af8c79f7219b2938808
NikolaIvanov0821 <ni.ivanov2008@gmail.com>
2025-03-31 20:05:52 +0300

    add user provider and user context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,23 +4,25 @@ import { Route, Routes } from "react-router"
 import Home from "./components/home/Home.jsx"
 import Register from "./components/register/Register.jsx"
 import Login from "./components/login/Login.jsx"
+import UserProvider from "./providers/UserProvider.js"
 
 
 function App() {
 
     return (
         <>
-            <Header />
+            <UserProvider>
+                <Header />
 
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
 
-            <Routes>
-                <Route index element={<Home/>} />
-                <Route path="/register" element={<Register/>} />
-                <Route path="/login" element={<Login/>} />
-            </Routes>
+                <Footer />
+            </UserProvider>
 
-
-            <Footer />
         </>
     )
 }
